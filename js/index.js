f9da2ca48e2cac9f629ec81d0a8bdf240b07aa5b
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2020-12-16 13:21:14 +0100

    test

---
+++ js/index.js
@@ -0,0 +1,130 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10); 
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let country ="Norway";
+let continent ="Europe";
+let population ="5 million";
+let isIsland ="Island";
+let language =""
+
+console.log(country);
+console.log(continent);
+console.log(population); 
+console.log(isIsland);
+console.log(typeof isIsland); 
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1 
+x--;
+console.log(x); 
+
+// Comparison operator 
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018); */
+
+//Coding Challenge: 
+
+const result = ("BMI");
+const massMark = 95;
+const heightMark = 1.88;
+
+const massJohn = 85;
+const heightJohn = 1.76;
+
+console.log((massMark / heightMark ** 2) +" "+ result);
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI); 
+
+if(BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!" )
+   } else {
+    console.log("John's BMI is higher than Mark's!")
+    }
+
+
+/*
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonasNew =`I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`just a regular string....`)
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple 
+lines`); 
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if(isOldEnough) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012; 
+
+let century;
+if (birthYear <= 2000) {
+     century = 20;
+} else {
+     century = 21;
+}
+console.log(century); */
+
