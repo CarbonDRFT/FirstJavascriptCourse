cb0882b4b2d3140704a8a42292978fcfc461d7c3
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2021-01-26 10:51:08 +0100

    update

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018); */
 
-//Coding Challenge: 
+//Coding Challenge:
 
 /*const result = ("BMI");
 const massMark = 95;
@@ -303,62 +303,192 @@ console.log(friends.indexOf('Steven'));
 console.log(friends.includes('Steven'))
 console.log(friends.includes('Bob')) */
 
-
 // OBJECT
 
 // Object gives us the possibility to add names to the value, give them name to each value
-// which was not possible with arrays. 
+// which was not possible with arrays.
 // So we are making propperties inside the curly brackets in object
 // Object is one of the most fundamental part of the whole Javascript
-// We are able to group together variables that really belong together. 
-// Big difference between Arrays and Objects is that, the order of the values in object does not matter 
+// We are able to group together variables that really belong together.
+// Big difference between Arrays and Objects is that, the order of the values in object does not matter
 // at all when we want to retrieve them. In Array, the order which we specify, matters a lot, because
-// thats how we access the values. 
+// thats how we access the values.
 // We should use array in more order data, while Objects be used in unstructure data
 
+// const danierArray = [
+//     'Danier' ,
+//     'Valiev' ,
+//     2020 - 1995 ,
+//     'Student' ,
+//     ['John' , 'Eric' , 'Aimee']
+// ]
 
-const danierArray = [
-    'Danier' , 
-    'Valiev' , 
-    2020 - 1995 , 
-    'Student' , 
-    ['John' , 'Eric' , 'Aimee']
-]
+// const danier = {
+//     firstName:  'Danier',
+//     lastName: 'Valiev',
+//     age: 2020 - 1996 ,
+//     job: 'Student',
+//     friends: ['John' , 'Eric', 'Aimee'],
 
-const danier = {
-    firstName:  'Danier',
-    lastName: 'Valiev',
-    age: 2020 - 1996 , 
-    job: 'Student', 
-    friends: ['John' , 'Eric', 'Aimee'],
-    
+// }
+
+// console.log(danier);
+
+// console.log(danier.lastName);
+// console.log(danier['lastName'])
+
+// const nameKey = 'Name';
+// console.log(danier['first' + nameKey])
+// console.log(danier['last' + nameKey])
+
+// // console.log(danier.'last' + nameKey) does not work.
+
+// const interestedIn = prompt('What do you want to know about Danier? Choose between firstName, lastName, age, job, and friends')
+
+// if (danier[interestedIn]) {
+//     console.log(danier[interestedIn])
+// } else {
+//     console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+// }
+
+// danier.location = 'Norway'
+// danier['instagram'] = 'Danier95'
+
+// console.log(danier);
+
+// //Challenge
+// // Jonas has 3 friends, and his best friend is called Aimee
+
+// console.log(`${danier.firstName} has ${danier.friends.length} friends, and his girlfriend is named ${danier.friends[2]}`);
+
+// WHILE LOOP
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`)
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+//     console.log(`While: Lifting weights repetition ${rep}`);
+//     rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+// while (dice !== 6) {
+//     console.log(`You rolled a ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) console.log('You got 6. Loop ended...')
+// }
+
+// Coding Challenge 1 - Javascript 2.
+
+/*   
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+Hints:
+§ To calculate average of 3 values, add them all together and divide by 3
+§ To check if number A is at least double number B, check for A >= 2 * B.
+Apply this to the team's average scores �
+GOOD LUCK
+*/
+
+//Test 1
+
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas)
+
+// const checkWinner = function(avgDolphins, avgKoalas) {
+//     if (avgDolphins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
+//     } else if (avgKoalas >= 2 * avgDolphins) {
+//         console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
+//     } else {
+//         console.log('No teams wins...')
+//     }
+// }
+
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// checkWinner(100, 200);
+
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+('use strict');
+
+const x = 23;
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log();
+
+const fiveNumbers = [5, 10, 15, 20, 25];
+
+for (var i = 0; i < fiveNumbers.length; i++) {
+  console.log(fiveNumbers[i], typeof fiveNumbers[i]);
 }
 
-console.log(danier);
-
-console.log(danier.lastName);
-console.log(danier['lastName'])
-
-const nameKey = 'Name';
-console.log(danier['first' + nameKey])
-console.log(danier['last' + nameKey])
-
-// console.log(danier.'last' + nameKey) does not work. 
-
-const interestedIn = prompt('What do you want to know about Danier? Choose between firstName, lastName, age, job, and friends')
-
-if (danier[interestedIn]) {
-    console.log(danier[interestedIn])
-} else {
-    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+for (var randomNumbers = 15; randomNumbers <= 25; randomNumbers++) {
+  console.log([randomNumbers], typeof [randomNumbers]);
 }
 
-danier.location = 'Norway'
-danier['instagram'] = 'Danier95'
+console.log('--- ONLY NUMBER 20 ---');
+for (var randomNumbers = 15; randomNumbers <= 25; randomNumbers++) {
+  if (randomNumbers !== 20) continue;
+  console.log([randomNumbers]);
+}
 
-console.log(danier);
+let driverLicense = {
+  driver: [
+    {
+      personsName: 'Thomas',
+      age: 18,
+      canDrive: true,
+    },
+    {
+      personsName: 'Lina',
+      age: 16,
+      canDrive: false,
+    },
+  ],
+};
 
-//Challenge 
-// Jonas has 3 friends, and his best friend is called Aimee
+driverLicense.driver.forEach((obj, i) => {
+  console.log('Drivers name:', obj.personsName);
+  console.log('Can the person drive?', obj.canDrive);
+});
 
-console.log(`${danier.firstName} has ${danier.friends.length} friends, and his girlfriend is named ${danier.friends[2]}`);
+function whatIDontLike(food) {
+  console.log(`I don't like ${food}`);
+}
+
+whatIDontLike('browncheese');
+
+function answer(firstArgument, secondArgument) {
+  console.log(secondArgument - firstArgument);
+}
+answer(3, 5);
